fdecff1422854bf8801e0a33f86fd3741842061e
gkarumbi <gkarumbi@gmail.com>
2020-07-25 16:29:31 +0300

    Add route to writing a new blog

---
+++ app/blog/views.py
@@ -0,0 +1,27 @@
+from flask import render_template, redirect, request, url_for, abort
+from flask_login import login_required, current_user
+from ..models import User
+from .forms import BlogForm, CommentForm
+from . import blog
+from ..models import Blog, Comment
+from .. import db
+
+@blog.route('/blog/new', methods=['GET', 'POST'])
+@login_required
+def new_blog():
+
+    form = BlogForm()
+
+    if form.validate_on_submit():
+
+        title = form.title.data
+        
+        blog = form.blog.data
+
+        new_blog = Blog(blog=blog, title=title)
+
+        new_blog.save_blog()
+
+        return redirect(url_for('main.index'))
+
+    return render_template('new_blog.html', blog_form=form)
