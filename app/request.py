480932ca0e2d1c8c91fbc5d3a4d09f55826646db
gkarumbi <gkarumbi@gmail.com>
2020-07-25 16:00:50 +0300

    Add function to process our json object from a dictionary to a quote object for easier rendering in the views object

--- app/request.py
+++ app/request.py
@@ -1,5 +1,6 @@
 import urllib.request,json
 from .models import Quote
+from flask import jsonify
 
 
 def get_quotes():
@@ -7,19 +8,23 @@ def get_quotes():
     Function that fetches the JSON object
     '''
     get_quotes_url = 'http://quotes.stormconsultancy.co.uk/random.json'
+    print(get_quotes_url)
 
     with urllib.request.urlopen(get_quotes_url) as url:
         get_quotes_data = url.read()
         get_quotes_response = json.loads(get_quotes_data)
-
+        print(get_quotes_response)
         quote_results = None
 
         if get_quotes_response:
-            quote_results_list = get_quotes_response[]
+            print(get_quotes_response)
+            quote_results_list = get_quotes_response
             quote_results = process_results(quote_results_list)
+            
 
 
-    return quote_results
+    return quote_results 
+    
 
 
 def process_results(quote_list):
@@ -33,14 +38,18 @@ def process_results(quote_list):
         Quote_results: A list of QQuote objects
     '''
     quote_results = []
-    for quote_item in quote_list:
-        author= quote_item.get('author')
-        id = quote_item.get('id')
-        quote= quote_item.get('quote')
-        url = quote_item.get('permalink')
+
+    quote_item = quote_list
+
+    print(quote_item)
+    
+    author= quote_item['author']
+    id = quote_item['id']
+    quote= quote_item['quote']
+    url = quote_item['permalink']
 
         
-        quote_object = Quote(author,id,quote,url)
-        quote_results.append(quote_object)
+    quote_object = Quote(author,id,quote,url)
+    quote_results.append(quote_object)
 
     return quote_results
